c817a405554cdc2d7d08dee744971e5069065648
Park yejin <92977621+yezzan9@users.noreply.github.com>
2024-12-26 15:15:03 +0900

    feat: url에서 추출하여 링크 생 성

--- src/components/result/ShareList.tsx
+++ src/components/result/ShareList.tsx
@@ -1,26 +1,9 @@
-import { useEffect } from 'react';
-import ClipboardJS from 'clipboard';
+import CopyLinkButton from './CopyLinkButton';
 
 const ShareList = () => {
-  useEffect(() => {
-    const clipboard = new ClipboardJS('[data-clipboard-text]');
-
-    clipboard.on('success', () => {
-      console.log('성공');
-    });
-
-    clipboard.on('error', () => {
-      console.log('실패');
-    });
-
-    return () => {
-      clipboard.destroy();
-    };
-  }, []);
-
   return (
     <div>
-      <button data-clipboard-text="복사 테스트">링크복사</button>
+      <CopyLinkButton />
       <button>카톡</button>
       <button>QR</button>
     </div>
