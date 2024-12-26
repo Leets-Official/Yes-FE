6e8d269a5b436c02c37e81573433f19d2ccb61de
Park yejin <92977621+yezzan9@users.noreply.github.com>
2024-12-26 14:24:42 +0900

    feat: 링크 복사 기능 구현

--- src/components/result/ShareList.tsx
+++ src/components/result/ShareList.tsx
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
+import ClipboardJS from 'clipboard';
+
 const ShareList = () => {
+  useEffect(() => {
+    const clipboard = new ClipboardJS('[data-clipboard-text]');
+
+    clipboard.on('success', () => {
+      console.log('성공');
+    });
+
+    clipboard.on('error', () => {
+      console.log('실패');
+    });
+
+    return () => {
+      clipboard.destroy();
+    };
+  }, []);
+
   return (
     <div>
-      <button>링크</button>
+      <button data-clipboard-text="복사 테스트">링크복사</button>
       <button>카톡</button>
       <button>QR</button>
     </div>
