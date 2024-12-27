b99d24008ee20169adb349bf65aee104b10667f8
odukong <ohbin1017@gmail.com>
2024-12-27 22:09:25 +0900

    Refactor : Button 컴포넌트 재사용
    - Login페이지에서 사용되었던 KakaoLogin버튼을 삭제하고, 기본 Button 컴포넌트를 이용해 코드 재사용
    
    ref : #2

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,24 +1,8 @@
 import styled from 'styled-components';
 import LOGO from '../../assets/image/yes.svg'; // 로고 SVG 파일을 import
-import KakaoLoginButton from '../../components/login/KakaoLoginButton';
+import Button from '../../components/common/Button';
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  height: 100vh;
-  overflow-y: hidden;
-  box-sizing: border-box;
-`;
-
-const Logo = styled.img`
-  margin-top: 90%;
-  max-width: 100%;
-  height: auto;
-`;
-
-const Login = () => {
+export const Login = () => {
   // oauth 요청 URL
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${import.meta.env.VITE_KAKAO_REST_API}&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URI}`;
 
@@ -29,9 +13,33 @@ const Login = () => {
   return (
     <Container>
       <Logo src={LOGO} />
-      <KakaoLoginButton onClick={handleLogin} />
+      <KakaoLoginButton color="#ffe479" onClick={handleLogin}>
+        카카오로 로그인하기
+      </KakaoLoginButton>
     </Container>
   );
 };
 
-export default Login;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  max-width: 480px;
+  overflow-y: hidden;
+`;
+
+const Logo = styled.img`
+  margin-top: 90%;
+  width: 100%;
+  max-width: 200px;
+  height: auto;
+`;
+
+const KakaoLoginButton = styled(Button)`
+  width: 80%;
+  margin: 2rem;
+  font-weight: 400;
+`;
