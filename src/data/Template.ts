3c76a64fbecb2bbe493abd55371add451962d2df
odukong <ohbin1017@gmail.com>
2025-01-04 18:28:51 +0900

    style : 템플릿 예시 데이터 파일 작성
    - template 예시 데이터 (src, text_cnt, position, color)
    - 예시 템플릿 이미지 업로드

---
+++ src/data/Template.ts
@@ -0,0 +1,17 @@
+export const template = {
+  EXAMPLE: {
+    // 1. 템플릿 데이터 src
+    template_src: '../assets/image/template_example.png',
+    // 2. 텍스트 개수
+    text_cnt: 2,
+    // 3. 각 텍스트 위치 (top,left)
+    text_position: [
+      [59, 133],
+      [119, 47],
+    ],
+    // 4. 템플릿 컬러(뒷면)
+    bg_color: '#FF8383',
+    // 5. 텍스트 컬러(뒷면)
+    bg_text_color: '#FFFFFF',
+  },
+};
