154950b2dce59a5e82c2394db5263a1bb0ea3c37
odukong <ohbin1017@gmail.com>
2025-01-06 16:00:43 +0900

    style : 예시 데이터 추가
    - 템플릿 선택 기능 확인을 위한 템플릿 이미지 추가
    
    ref : #6

--- src/data/Template.ts
+++ src/data/Template.ts
@@ -14,4 +14,19 @@ export const template = {
     // 5. 텍스트 컬러(뒷면)
     bg_text_color: '#FFFFFF',
   },
+  EXAMPLE2: {
+    // 1. 템플릿 데이터 src
+    template_src: '/image/template_example2.png',
+    // 2. 텍스트 개수
+    text_cnt: 2,
+    // 3. 각 텍스트 위치 (top,left)
+    text_position: [
+      [174, 179],
+      [179, 367],
+    ],
+    // 4. 템플릿 컬러(뒷면)
+    bg_color: '#FFFFCC',
+    // 5. 텍스트 컬러(뒷면)
+    bg_text_color: '#F57F17',
+  },
 };
