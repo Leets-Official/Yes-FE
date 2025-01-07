3506a543ffe3b055796f8c989a07e27c6b480bca
odukong <ohbin1017@gmail.com>
2025-01-07 15:18:47 +0900

    feat :초대장 정보 recoil 상태관리 초기 코드
    -  Recoil 상태 관리 파일 작성 : InvitationInfo.ts
    - RecoilRoot로 App 컴포넌트를 감싸며 전역으로 Recoil을 사용가능하도록 함.
    
    ref : #6

---
+++ src/atom/InvitationInfo.ts
@@ -0,0 +1,50 @@
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist({ key: 'invitationPersist', storage: localStorage });
+
+/**
+ * [ 초대장 정보 recoil Atom]
+ * step : 초대장 입력 단계
+ * nickname : 닉네임
+ * isTemplate : 초대장 템플릿 사용여부
+ * templeteKey : 템플릿 키 (선택)
+ * imageFile : 이미지(=템플릿) 파일 (선택)
+ * imageUrl : 미리보기 용 이미지 url (선택)
+ * contents[] : 템플릿 내 텍스트 내용 (선택)
+ * title : 초대장 제목
+ * date : 초대장 일자  (YYYY-MM-DD-HH-MM)
+ * location : 초대장 장소
+ * description : 초대장 부가 내용
+ */
+export interface InvitationState {
+  step: number;
+  nickname: string;
+  isTemplate: boolean;
+  templateKey?: string;
+  imageFile?: File;
+  imageUrl?: string;
+  contents: string[];
+  title: string;
+  date: string;
+  location: string;
+  description: string;
+}
+
+export const InvitationInfo = atom<InvitationState>({
+  key: 'invitationInfo', // atom 고유 키 (local key)
+  default: {
+    step: 0,
+    nickname: '',
+    isTemplate: true,
+    templateKey: 'EXAMPLE',
+    imageFile: undefined,
+    imageUrl: '',
+    contents: [],
+    title: '',
+    date: '',
+    location: '',
+    description: '',
+  },
+  effects_UNSTABLE: [persistAtom], // persistAtom을 통해 상태 지속화
+});
