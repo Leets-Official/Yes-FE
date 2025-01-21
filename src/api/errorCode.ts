13ce743a4efe441e45f4f4b5551d980a4d74e516
odukong <ohbin1017@gmail.com>
2025-01-21 23:52:52 +0900

    feat : 500(서버, 네트워크) 에러 코드 매핑 파일 추가
    - 500 코드 중 에러를 세분화(error.code에 근거)하여 error-component에서 보여질 각각의 message, 버튼 text, 버튼 action을 작성함.
    
    ref : #25

---
+++ src/api/errorCode.ts
@@ -0,0 +1,21 @@
+export const errorCode = (code: string) => {
+  // 500 (서버 에러 발생 시, error-page에 적용될 객체)
+  const errorMap = {
+    INTERNAL_SERVER_ERROR: {
+      message: '서버 오류가 발생했습니다.',
+      btn_message: '새로고침',
+      action: () => window.location.reload(),
+    },
+    default: {
+      message: '예상치 못한 에러가 발생했습니다.',
+      btn_message: '다시시도',
+      action: () => window.location.reload(),
+    },
+  };
+
+  if (code in errorMap) {
+    return errorMap[code as keyof typeof errorMap];
+  }
+
+  return errorMap.default;
+};
