29a07e9fb156ede3e0c5d11142055e6cb1e1bd03
odukong <ohbin1017@gmail.com>
2025-01-21 23:59:42 +0900

    feat : 공통 API Axios 에러 컴포넌트 구현 (+ API 작성 예시 코드)
    - axios 통신 중 에러 발생 시,  보여질 컴포넌트를 작성하였습니다.
    - ex. fetchData.ts 통신 함수로 API 연결 시 작성되어야 할 코드를 문서화하였습니다. (삭제 예정)
    
    ref : #25

--- src/pages/ErrorFallback.tsx
+++ src/pages/ErrorFallback.tsx
@@ -1,5 +1,14 @@
-const ErrorFallback = ({ error, resetErrorBoundary }: any) => {
-  return <div onClick={resetErrorBoundary}>{error}</div>;
+import DefaultError from '../components/error/DefaultError';
+import { errorCode } from '../api/errorCode';
+
+interface FallbackUIProps {
+  error: any; // 실제 에러 객체
+  resetErrorBoundary: () => void; // 에러 초기화 함수 (사용 X)
+}
+
+const ErrorFallback = ({ error }: FallbackUIProps) => {
+  const { message, btn_message, action } = errorCode(error.name);
+  return <DefaultError message={message} btnText={btn_message} onClick={action} />;
 };
 
 export default ErrorFallback;
