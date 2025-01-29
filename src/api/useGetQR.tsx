b5b1e3c256215f8199c328cea693c69e420c580e
Park yejin <92977621+yezzan9@users.noreply.github.com>
2025-01-30 00:09:05 +0900

    feat: error state 제거

--- src/api/useGetQR.tsx
+++ src/api/useGetQR.tsx
@@ -6,7 +6,6 @@ import { UserInfo } from '../atom/UserInfo';
 
 export const useGetQR = (invitationId: string) => {
   const [data, setData] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
 
   const { showBoundary } = useErrorBoundary(); // 전역 에러 처리
   const resetUserInfo = useResetRecoilState(UserInfo); // 토큰 만료 시 유저 정보 초기화
@@ -21,7 +20,6 @@ export const useGetQR = (invitationId: string) => {
         });
 
         setData(response.data.result.qrUrl);
-        setError(null);
       } catch (error: any) {
         if (error.name !== 'GENERAL') {
           showBoundary(error); // 500 에러 → 전역 에러 핸들링
@@ -34,7 +32,7 @@ export const useGetQR = (invitationId: string) => {
     fetchData();
   }, [invitationId]);
 
-  return { data, error };
+  return { data };
 };
 
 export default useGetQR;
